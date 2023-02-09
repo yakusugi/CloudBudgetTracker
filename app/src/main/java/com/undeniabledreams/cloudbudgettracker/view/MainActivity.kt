4229697ca9528bd8bdc499e9bd89adc0cb869cf5
Yakusugi <yosukeito1987@gmail.com>
2023-02-09 17:44:27 +0900

    commit feb 9 2023 bottom navigation view

--- app/src/main/java/com/undeniabledreams/cloudbudgettracker/view/MainActivity.kt
+++ app/src/main/java/com/undeniabledreams/cloudbudgettracker/view/MainActivity.kt
@@ -2,11 +2,42 @@ package com.undeniabledreams.cloudbudgettracker.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.undeniabledreams.cloudbudgettracker.R
+import com.undeniabledreams.cloudbudgettracker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(DashFragment())
+
+        binding.bottomNavView.setOnItemSelectedListener {
+
+            when(it.itemId) {
+                R.id.dash -> replaceFragment(DashFragment())
+                R.id.stats -> replaceFragment(StatsFragment())
+                R.id.search -> replaceFragment(SearchFragment())
+                R.id.convert -> replaceFragment(ConverterFragment())
+
+                else -> {
+
+                }
+            }
+            true
+        }
+
     }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
+    }
+
 }
